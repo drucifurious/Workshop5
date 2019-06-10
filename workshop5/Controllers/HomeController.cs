46a92e887a7e81197cc6a5ecbba65d00bd2eb0d8
liuye5 <liuye5@users.noreply.github.com>
2019-06-10 16:06:39 -0600

    ye

--- workshop5/Controllers/HomeController.cs
+++ workshop5/Controllers/HomeController.cs
@@ -76,5 +76,26 @@ namespace workshop5.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CustomerId,CustFirstName,CustLastName,CustAddress,CustCity,CustProv,CustPostal,CustCountry,CustHomePhone,CustBusPhone,CustEmail,AgentId,PasswordNotHashed,PasswordHashed,PasswordSalt")] Customers customers)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(customers);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customers);
+        }
     }
 }
