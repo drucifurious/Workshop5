8d74fd58b85080cf63d4243fc9791ae975affcd2
liam34 <48293076+liam34@users.noreply.github.com>
2019-06-11 16:23:52 -0600

    liam changes tuesday 4:23

--- workshop5/Controllers/HomeController.cs
+++ workshop5/Controllers/HomeController.cs
@@ -50,27 +50,14 @@ namespace workshop5.Controllers
 
             return View();
         }
-
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
+   
         public IActionResult Packages()
         {
             ViewData["Message"] = "Your Packages page.";
 
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
