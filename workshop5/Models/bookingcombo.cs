695fc41e9121dd0f63bf64c3ce87eb9a6749b283
Drew <carldrewlamb@gmail.com>
2019-06-12 10:06:22 -0600

    drew commit

---
+++ workshop5/Models/bookingcombo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace workshop5.Models
+{
+    public class bookingcombo
+    {
+        public Bookings Bookings { get; set; }
+        public CreditCards CreditCards { get; set; }
+    }
+}
