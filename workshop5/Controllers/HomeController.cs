4a305f274ee864f161baa543b5c16b58a7cd3331
liam34 <48293076+liam34@users.noreply.github.com>
2019-06-12 16:22:05 -0600

    liam changes 4:22

--- workshop5/Controllers/HomeController.cs
+++ workshop5/Controllers/HomeController.cs
@@ -57,7 +57,16 @@ namespace workshop5.Controllers
 
             return View();
         }
-        
+        public IActionResult Chinese()
+        {
+            ViewData["Message"] = "Chinese Language Available.";
+
+            return View();
+        }
+
+     
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
